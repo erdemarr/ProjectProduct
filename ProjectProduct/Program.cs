8b14e711837ce807a677dcd4bbd0d9a39c71f866
TAMİR-HANE <TAMR-HANE@DESKTOP-GQPUVRI>
2024-07-25 09:29:28 +0300

    Proje dosyası ekle.

---
+++ ProjectProduct/Program.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CategoryManager cm = new CategoryManager();
+
+            ProductManager pm = new ProductManager();
+            foreach(var item in pm.GetAll())
+            {
+                Console.WriteLine("ID: " + item.ProductID + " Ürün Adı: " + item.ProductName + " Stok: " + item.Stock);
+            }
+            Console.WriteLine();
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+
+            string proname = "Fırın";
+            foreach(var item in pm.GetByName(proname))
+            {
+                Console.WriteLine("ID: " + item.ProductID + " Ürün Adı: " + item.ProductName + " Stok: " + item.Stock);
+            }
+
+            //Category c = new Category();
+            //c.CategoryID = 3;
+            //c.CategoryName = "Mobilya ve Oturma Grubu";
+            //cm.BLUpdate(c);
+
+            //foreach (var item in cm.GetAll())
+            //{
+            //    Console.WriteLine("ID: " + item.CategoryID + " - Kategori Adı: " + item.CategoryName);
+            //}
+            //Category ct = new Category();
+            //ct.CategoryName = "Halılar";
+            //cm.BLAdd(ct);
+            //cm.BLDelete(6);
+            Console.Read();
+        }
+    }
+}
---
+++ ProjectProduct/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ProjectProduct")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectProduct")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("6a4e9d4f-46e0-4a17-857d-92b49191ba4d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
